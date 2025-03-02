a68686ceeb094a322abc5a95918089de5d6ff4ae
harshitharajesh19 <166856837+harshitharajesh19@users.noreply.github.com>
2025-03-02 16:06:29 +0530

    userRoutes.js

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,1 +1,13 @@
+import express from "express";
+import { createUser,deleteUser, getAllUsers, getUser, updateUser, loginUser } from "../controllers/userController.js";
 
+const userRoutes = express.Router();
+
+userRoutes.get("/", getAllUsers);
+userRoutes.post("/login", loginUser);
+userRoutes.post("/", createUser);
+userRoutes.get("/:id", getUser);
+userRoutes.put("/:id", updateUser);
+userRoutes.delete("/:id", deleteUser); 
+
+export default userRoutes;
