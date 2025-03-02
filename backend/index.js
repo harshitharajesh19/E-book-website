64e354537fd1a9b6beee2ed515672a83732f7cf9
harshitharajesh19 <166856837+harshitharajesh19@users.noreply.github.com>
2025-03-02 21:49:06 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,31 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import connectDatabase from "./src/database.js";
+import bookRoutes from './src/routes/bookRoutes.js';
+import userRoutes from './src/routes/userRoutes.js';
+// import cartRoutes from './src/routes/cartRoutes.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(cors());
+// Connect to the database
+const startServer = async () => {
+  try {
+    await connectDatabase(); // Ensure the database is connected
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Failed to start server:", err);
+  }
+};
+startServer();
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use("/books", bookRoutes);
+app.use("/users",userRoutes);
+
+
 
